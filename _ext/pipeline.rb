77edd937622bbd9d67d4fecae57e668bc9d2824c
Maxime Gréau <contact@mgreau.com>
2013-08-21 23:17:08 +0200

    Jason Lee post about awestruct/asciidoc blog

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,8 +1,17 @@
 require 'bootstrap-sass'
+require 'erubis'
+require 'tilt'
 
 Awestruct::Extensions::Pipeline.new do
-  # extension Awestruct::Extensions::Posts.new '/news'
-  # extension Awestruct::Extensions::Indexifier.new
-  # Indexifier *must* come before Atomizer
-  # extension Awestruct::Extensions::Atomizer.new :posts, '/feed.atom'
+  extension Awestruct::Extensions::Posts.new( '/posts', :posts)
+  extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page => 10 )
+  extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/posts/tags', :per_page => 10)
+  extension Awestruct::Extensions::TagCloud.new( :tagcloud, '/posts/tags/index.html', :layout=>'base', :title=>'Tags')
+  extension Awestruct::Extensions::Disqus.new
+
+  extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/feed.atom', :feed_title=>'mgreau Blog' )
+
+  helper Awestruct::Extensions::Partial
+  helper Awestruct::Extensions::GoogleAnalytics
 end
