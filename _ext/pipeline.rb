30fa67dcc424286e4ded181ca694a362e52958d7
Maxime Gréau <contact@mgreau.com>
2013-08-21 23:56:09 +0200

    delete eribis pb

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,5 +1,5 @@
 require 'bootstrap-sass'
-require 'erubis'
+#require 'erubis'
 require 'tilt'
 
 Awestruct::Extensions::Pipeline.new do
@@ -7,7 +7,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page => 10 )
   extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/posts/tags', :per_page => 10)
   extension Awestruct::Extensions::TagCloud.new( :tagcloud, '/posts/tags/index.html', :layout=>'base', :title=>'Tags')
-  extension Awestruct::Extensions::Disqus.new
+ # extension Awestruct::Extensions::Disqus.new
 
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Atomizer.new( :posts, '/feed.atom', :feed_title=>'mgreau Blog' )
