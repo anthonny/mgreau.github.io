43397b30138e0d171009dbf02b516035a92e0b23
Maxime Gréau <contact@mgreau.com>
2013-10-19 12:10:18 +0200

    add experience + blog post in PDF generate with asciidoctor

--- javascripts/script.js
+++ javascripts/script.js
@@ -213,6 +213,14 @@ $(document).ready(function() {
       $('#work .item a').removeClass('flex-active');
     });
   }
+
+    window.___gcfg = {lang: 'fr'};
+
+    (function() {
+      var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+      po.src = 'https://apis.google.com/js/plusone.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    })();
   
   
 });
